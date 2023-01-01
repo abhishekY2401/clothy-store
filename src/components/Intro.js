776d19a97158bcd519427517512f3b4460098010
Abhishek Yadav <abhiyd2401@gmail.com>
2023-01-01 19:01:38 +0530

    created stunning intros for app

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -3,25 +3,34 @@ import "./Intro.css";
 
 const Intro = () => {
   return (
-    <div className="clothy__intro">
-      <div className="matte_grey">
-        <img src={sand} alt="matte" className="greyy" />
+    <>
+      <div className="clothy__intro">
+        <div className="clothy__info">
+          <div className="design__quote">
+            <div id="slant__line"></div>
+            <span id="style">
+              what you wear <br />
+              speaks volumes
+              <br /> about who you are
+            </span>
+            <div id="slant__line"></div>
+          </div>
+
+          <div className="about__clothy">
+            <span id="about">
+              We at clothz offer
+              <br /> you wide varieties of
+              <br /> clothes which can be
+              <br /> fully customized and
+              <br /> are comfortable to wear.
+            </span>
+          </div>
+        </div>
+        <div className="matte_grey">
+          <img src={sand} alt="matte" className="greyy" />
+        </div>
       </div>
-      <div className="design__quote">
-        <span id="style">Style</span> is a <span id="better">better</span>
-        <br /> way to
-        <span id="speak">speak</span>
-      </div>
-      <div className="about__clothy">
-        <span>
-          We at clothz provide
-          <br /> you wide varieties of
-          <br /> clothes which can be
-          <br /> fully customized and
-          <br /> are comfortable to wear.
-        </span>
-      </div>
-    </div>
+    </>
   );
 };
 
