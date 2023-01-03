6b157fd541c052df87ad4034aaf20bf50379a327
Abhishek Yadav <abhiyd2401@gmail.com>
2023-01-03 22:44:29 +0530

    implemented a trending section

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -1,13 +1,39 @@
-import React from "react";
+import "./trending.css";
+import blue from "../assets/trending/blue_tee.svg";
+import peach from "../assets/trending/peach_tee.svg";
+import green from "../assets/trending/green_tee.svg";
 
 const Trending = () => {
   return (
-    <div>
+    <div className="trending">
       <div className="trending__section">
         <div>Trending Wears</div>
       </div>
-      <div>
-        <div></div>
+      <div className="trending__clothes">
+        <div>
+          <img src={blue} alt="blue_tee" className="blue__tee"/>
+          <div>
+            <div>Khaki green</div>
+            <div>Long T-shirt</div>
+            <div>Rs. 1,559.00</div>
+          </div>
+        </div>
+        <div>
+          <img src={peach} alt="peach_tee" />
+          <div>
+            <div>Khaki green</div>
+            <div>Long T-shirt</div>
+            <div>Rs. 1,559.00</div>
+          </div>
+        </div>
+        <div>
+          <img src={green} alt="green_tee" />
+          <div>
+            <div>Khaki green</div>
+            <div>Long T-shirt</div>
+            <div>Rs. 1,559.00</div>
+          </div>
+        </div>
       </div>
     </div>
   );
